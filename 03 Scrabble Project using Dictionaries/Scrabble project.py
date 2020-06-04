b5d86697057833b134caff24e18c3b14c886ed08
Somasale Balaji <35003840+balajisomasale@users.noreply.github.com>
2020-06-04 17:19:16 +0530

    using dictionaries

--- 03 Scrabble Project using Dictionaries/Scrabble project.py
+++ 03 Scrabble Project using Dictionaries/Scrabble project.py
@@ -1,7 +1,7 @@
 '''
 play_word() — a function that would take in a player and a word, and add that word to the list of words they’ve played
 update_point_totals() — turn your nested loops into a function that you can call any time a word is played
-make your letter_to_points dictionary able to handle lowercase inputs as well'''
+make your letter_to_points dictionary able to handle  lowercase inputs as well'''
 
 
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
